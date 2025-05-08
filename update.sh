34e44d3a5698914075d7d264a8c477beba1c70ae
cass <casstanje.pm.me>
2025-05-08 17:19:31 +0200

    Added a wizard to the update.sh script

--- update.sh
+++ update.sh
@@ -1,47 +1,102 @@
 #!/bin/bash
-declare -a config_files=()
-declare -a local_share_files=()
+# Get current path for later
+startPath=$(pwd)
 
-rm -rf config/*
-rm -rf local_share/*
+# Get save savePath
+echo "Where do you want to store this stuff?"
+echo "(Leave empty to save in current folder)"
+read -p "" -r savePath
+if [ "$savePath" != "" ]
+then
+    eval cd $savePath
+fi
 
-echo "Looking for .config folders..."
-for dir in ~/.config/*
-do
-    name=$(basename $dir)
+# config
+read -p "Save files from ~/.config? (Yy/Nn)" -n 1 -r
+echo
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+    # Delete the current config files, if the user wants it
+    read -p "Delete your currently saved ~/.config files first? (Yy/Nn)" -n 1 -r
+    echo
+    if [[ $REPLY =~ ^[Yy]$ ]]
+    then
+        rm -rf config/*
+    fi
 
-    containsValue="false"
-    for i in "${config_files[@]}"
+    # Save an array of all the files in the folder and echo them
+    index=0
+    declare -a listOfItems
+    echo "Folders and files in ~/.config:"
+    for file in ~/.config/*
     do
-        if [[ ${i//,} == $name ]];
-        then
-            echo "Found file/folder: ${name}"
-            containsValue="true"
-        fi
+        name=$(basename $file)
+        index=$((++index))
+        echo "${index}. ${name}"
+        listOfItems["$index"]="$name"
     done
 
-    if [ $containsValue == "true" ]; then
-        cp -r $dir config
+    # Get the files to be saved and save them
+    echo
+    echo "-"
+    echo "List the files you'd like to save in a comma-seperated numbered list"
+    echo "e.g. \"65,1,2,3,6\""
+    echo "(Leave blank if none)"
+    read -p "" list
+    echo
+    if [ "$list" != "" ]; then
+        IFS=',' read -r -a listArray <<< "$list"
+        for element in "${listArray[@]}"
+        do
+            echo "Saving ${listOfItems[${element}]}..."
+            cp -r ~/.config/${listOfItems[${element}]} config
+        done
     fi
-done
+fi
 
-echo ''
-echo "Looking for .local/share folders..."
-for dir in ~/.local/share/*
-do
-    name=$(basename $dir)
+# local/share
+echo
+echo "-"
+read -p "Save files from ~/.local/share? (Yy/Nn)" -n 1 -r
+echo
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+    # Delete the current config files, if the user wants it
+    read -p "Delete your currently saved ~/.local/share files first? (Yy/Nn)" -n 1 -r
+    echo
+    if [[ $REPLY =~ ^[Yy]$ ]]
+    then
+        rm -rf local_share/*
+    fi
 
-    containsValue="false"
-    for i in "${local_share_files[@]}"
+    # Save an array of all the files in the folder and echo them
+    index=0
+    declare -a listOfItems
+    echo "Folders and files in ~/.local/share:"
+    for file in ~/.local/share/*
     do
-        if [[ ${i//,} == $name ]];
-        then
-            echo "Found file/folder: ${name}"
-            containsValue="true"
-        fi
+        name=$(basename $file)
+        index=$((++index))
+        echo "${index}. ${name}"
+        listOfItems["$index"]="$name"
     done
 
-    if [ $containsValue == "true" ]; then
-        cp -r $dir local_share
+    # Get the files to be saved and save them
+    echo
+    echo "-"
+    echo "List the files you'd like to save in a comma-seperated numbered list"
+    echo "e.g. \"65,1,2,3,6\""
+    echo "(Leave blank if none)"
+    read -p "" list
+    echo
+    if [ "$list" != "" ]; then
+        IFS=',' read -r -a listArray <<< "$list"
+        for element in "${listArray[@]}"
+        do
+            echo "Saving ${listOfItems[${element}]}..."
+            cp -r ~/.local/share/${listOfItems[${element}]} local_share
+        done
     fi
-done
+fi
+
+cd $startPath
